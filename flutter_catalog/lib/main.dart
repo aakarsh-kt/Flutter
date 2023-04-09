58fc9e6005a8f729c1020d800aeef81b8d25e023
Akarsh <akarshofficial009@gmail.com>
2023-04-09 19:53:12 +0530

    Day-2 Completed ,learned scafffold and dart

--- flutter_catalog/lib/main.dart
+++ flutter_catalog/lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Container(
-          child: Text("Welcome to flutter"),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
