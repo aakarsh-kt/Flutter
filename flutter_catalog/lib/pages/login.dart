6e059fd83754adebb106ab4ae38c02efaa66299a
Akarsh <akarshofficial009@gmail.com>
2023-04-10 23:12:17 +0530

    Day -5 ;Learned scroll view and how to navigate to different pages

--- flutter_catalog/lib/pages/login.dart
+++ flutter_catalog/lib/pages/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/utils/route.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,12 +8,58 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          Image.asset("assets/images/login_page.png"),
-        ],
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_page.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Welcome",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    child: Text("Login"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      /*child: Center(
+
+      /* child: Center(
         child: Text(
           "Login Page",
           style: TextStyle(
