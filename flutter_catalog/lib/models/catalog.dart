87f5e758faafb5be4cefeb0605e7832c27aec86f
Akarsh Katiyar <akarshofficial009@gmail.com>
2023-04-24 05:16:23 +0530

    Day 10 completed ; added models

---
+++ flutter_catalog/lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final String color;
+  final String img;
+  final num Price;
+
+  Item({this.id, this.name, this.desc, this.color, this.img, this.Price});
+}
+
+final product = [
+  Item(
+      id: "anubhav001",
+      name: "Iphone 12 Pro",
+      desc: "Apple iPhone 12th generation",
+      Price: 999,
+      color: "#33505a",
+      img:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+];
